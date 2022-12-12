ca9d3cabec7365935ca29c4940d55b74f1b48855
jyotir <jyotir@pop-os.localdomain>
2022-12-12 10:44:15 -0500

    created sidebar component and home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import {Route, Routes} from 'react-router-dom'
+import Home from "./components/pages/Home";
 
 
 function App() {
   return (
     <div className="App">
 <Navbar />
+
+<main>
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+</main>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <div>
+        <AiOutlineMenu size={30}/>
+        </div>
+        <h1>MyMov</h1>
+    </div>
   )
 }
 
