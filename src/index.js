e0a935e101b3547bdf1681eb845ba7f4956e1908
jyotir <jyotir@pop-os.localdomain>
2022-12-12 11:14:32 -0500

    created sidebar component and home page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+  createRoutesFromElements,
+} from "react-router-dom";
 
+import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
 );
 
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route element={<AppLayout />}>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/products" element={<Products />} />
+//       <Route path="/reports" element={<Reports />} />
+//     </Route>
+//   )
+// );
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      // {
+      //   path: "products",
+      //   element: <Products />,
+      // },
+      // {
+      //   path: "reports",
+      //   element: <Reports />,
+      // },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
 
